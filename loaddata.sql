1f92b2ac3d55e3c36fbffa78b21f77f5a42c90d4
Jaylen <jayrw6@gmail.com>
2025-05-06 20:28:55 -0500

    fixed syntax issue in sql file

--- loaddata.sql
+++ loaddata.sql
@@ -15,19 +15,18 @@ CREATE TABLE "DemotionQueue" (
   "action" varchar,
   "admin_id" INTEGER,
   "approver_one_id" INTEGER,
-  FOREIGN KEY(`admin_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`approver_one_id`) REFERENCES `Users`(`id`),
-  PRIMARY KEY (action, admin_id, approver_one_id)
+  FOREIGN KEY("admin_id") REFERENCES "Users"("id"),
+  FOREIGN KEY("approver_one_id") REFERENCES "Users"("id"),
+  PRIMARY KEY ("action", "admin_id", "approver_one_id")
 );
-
 
 CREATE TABLE "Subscriptions" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "follower_id" INTEGER,
   "author_id" INTEGER,
   "created_on" date,
-  FOREIGN KEY(`follower_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
+  FOREIGN KEY("follower_id") REFERENCES "Users"("id"),
+  FOREIGN KEY("author_id") REFERENCES "Users"("id")
 );
 
 CREATE TABLE "Posts" (
@@ -39,7 +38,7 @@ CREATE TABLE "Posts" (
   "image_url" varchar,
   "content" varchar,
   "approved" bit,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
+  FOREIGN KEY("user_id") REFERENCES "Users"("id")
 );
 
 CREATE TABLE "Comments" (
@@ -47,8 +46,8 @@ CREATE TABLE "Comments" (
   "post_id" INTEGER,
   "author_id" INTEGER,
   "content" varchar,
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
-  FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
+  FOREIGN KEY("post_id") REFERENCES "Posts"("id"),
+  FOREIGN KEY("author_id") REFERENCES "Users"("id")
 );
 
 CREATE TABLE "Reactions" (
@@ -62,9 +61,9 @@ CREATE TABLE "PostReactions" (
   "user_id" INTEGER,
   "reaction_id" INTEGER,
   "post_id" INTEGER,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`reaction_id`) REFERENCES `Reactions`(`id`),
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`)
+  FOREIGN KEY("user_id") REFERENCES "Users"("id"),
+  FOREIGN KEY("reaction_id") REFERENCES "Reactions"("id"),
+  FOREIGN KEY("post_id") REFERENCES "Posts"("id")
 );
 
 CREATE TABLE "Tags" (
@@ -76,8 +75,8 @@ CREATE TABLE "PostTags" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "post_id" INTEGER,
   "tag_id" INTEGER,
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
-  FOREIGN KEY(`tag_id`) REFERENCES `Tags`(`id`)
+  FOREIGN KEY("post_id") REFERENCES "Posts"("id"),
+  FOREIGN KEY("tag_id") REFERENCES "Tags"("id")
 );
 
 CREATE TABLE "Categories" (
@@ -85,6 +84,6 @@ CREATE TABLE "Categories" (
   "label" varchar
 );
 
-INSERT INTO Categories ('label') VALUES ('News');
-INSERT INTO Tags ('label') VALUES ('JavaScript');
-INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
+INSERT INTO "Categories" ("label") VALUES ('News');
+INSERT INTO "Tags" ("label") VALUES ('JavaScript');
+INSERT INTO "Reactions" ("label", "image_url") VALUES ('happy', 'https://pngtree.com/so/happy');
